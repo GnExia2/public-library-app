0fa3b933c2fef77c7a43c6b3820de9983798cf8f
Garrick Ngai <garrickdark@yahoo.com>
2017-08-28 10:04:47 -0700

    complete part 1

--- lib-app/config/routes.rb
+++ lib-app/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root to: 'users#index'
+
+  #users routes
+    get '/users/new', to: 'users#new', as: 'new_user'
+    post '/users', to: 'users#create'
+    get '/users/:id', to: 'users#show', as: 'user'
+
+  #login routes
+    get '/login', to: 'sessions#new'
+    get '/logout', to: 'sessions#destroy'
+    post '/sessions', to: 'sessions#create'
 end
